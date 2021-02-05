03b85a3bc84f28274f9a293e5122b87efd92a9d7
lqdev <liquidekgaming@gmail.com>
2021-02-05 14:44:28 +0100

    drawing.

--- src/netcanv.rs
+++ src/netcanv.rs
@@ -1,5 +1,5 @@
 use skulpin::*;
-use skulpin::app::{AppBuilder, AppHandler, AppUpdateArgs, AppDrawArgs, AppError};
+use skulpin::app::{AppHandler, AppUpdateArgs, AppDrawArgs, AppError, MouseButton};
 use skulpin::skia_safe::*;
 
 use crate::paint_canvas::*;
@@ -8,10 +8,14 @@ pub struct NetCanv<'a> {
     pub font_sans: Font,
     pub font_sans_bold: Font,
     pub paint_canvas: PaintCanvas<'a>,
+
+    previous_mouse: (f64, f64),
 }
 
 const SANS_TTF: &'static [u8] = include_bytes!("assets/fonts/Barlow-Medium.ttf");
 const SANS_BOLD_TTF: &'static [u8] = include_bytes!("assets/fonts/Barlow-Bold.ttf");
+
+const DEFAULT_CANVAS_SIZE: (u32, u32) = (1024, 600);
 
 impl NetCanv<'_> {
 
@@ -21,7 +25,8 @@ impl NetCanv<'_> {
         NetCanv {
             font_sans: Font::new(sans_typeface, 15.0),
             font_sans_bold: Font::new(sans_bold_typeface, 15.0),
-            paint_canvas: PaintCanvas::new((1024, 600)),
+            paint_canvas: PaintCanvas::new(DEFAULT_CANVAS_SIZE),
+            previous_mouse: (0.0, 0.0),
         }
     }
 
@@ -29,37 +34,34 @@ impl NetCanv<'_> {
 
 impl AppHandler for NetCanv<'_> {
 
-    fn update(&mut self, args: AppUpdateArgs) {
+    fn update(
+        &mut self,
+        AppUpdateArgs {
+            app_control: _,
+            input_state,
+            time_state: _,
+        }: AppUpdateArgs
+    ) {
+        let mouse: (f64, f64) = input_state.mouse_position().into();
+
+        if input_state.is_mouse_down(MouseButton::Left) {
+            self.paint_canvas.stroke(
+                (self.previous_mouse.0 as f32, self.previous_mouse.1 as f32),
+                (mouse.0 as f32, mouse.1 as f32),
+            );
+        }
+
+        self.previous_mouse = mouse;
     }
 
     fn draw(&mut self, args: AppDrawArgs) {
         let canvas = args.canvas;
         canvas.clear(Color::WHITE);
 
-        let black = Color4f::new(0.0, 0.0, 0.0, 1.0);
-        let mut black_fill = Paint::new(black, None);
-        black_fill.set_anti_alias(true);
-        black_fill.set_style(paint::Style::Fill);
-
-        canvas.draw_rect(Rect {
-            left: 32.0,
-            top: 32.0,
-            right: 64.0,
-            bottom: 64.0,
-        }, &black_fill);
-
-        canvas.draw_str(
-            "Regular",
-            Point::new(72.0, 32.0 + self.font_sans.size()),
-            &self.font_sans,
-            &black_fill,
-        );
-
-        canvas.draw_str(
-            "Bold",
-            Point::new(72.0, 48.0 + self.font_sans_bold.size()),
-            &self.font_sans_bold,
-            &black_fill,
+        canvas.draw_bitmap(
+            &self.paint_canvas,
+            (0.0, 0.0),
+            None,
         );
     }
 
--- src/paint_canvas.rs
+++ src/paint_canvas.rs
@@ -1,42 +1,42 @@
-use std::cell::RefCell;
 use std::ops::Deref;
-use std::rc::Rc;
 
 use skulpin::skia_safe::*;
 
 pub struct PaintCanvas<'a> {
-    bitmap: Rc<RefCell<Bitmap>>,
-    image: Image,
+    bitmap: Bitmap,
     canvas: OwnedCanvas<'a>,
 }
 
 impl PaintCanvas<'_> {
 
     pub fn new(size: (u32, u32)) -> Self {
-        let bitmap = Rc::new(RefCell::new(Bitmap::new()));
-        {
-            let mut mut_bitmap_ref = bitmap.borrow_mut();
-            mut_bitmap_ref.alloc_n32_pixels((size.0 as i32, size.1 as i32), false);
-        }
-        let bitmap_ref = bitmap.borrow();
-        let image = Image::from_bitmap(&bitmap_ref).unwrap();
-        let mut canvas = Canvas::from_bitmap(&bitmap_ref, None);
+        let mut bitmap = Bitmap::new();
+        bitmap.alloc_n32_pixels((size.0 as _, size.1 as _), None);
+        let mut canvas = Canvas::from_bitmap(&bitmap, None);
         canvas.clear(Color::TRANSPARENT);
         Self {
-            bitmap: bitmap.clone(),
-            image,
+            bitmap,
             canvas,
         }
+    }
+
+    pub fn stroke(&mut self, from: impl Into<Point>, to: impl Into<Point>) {
+        let mut paint = Paint::new(Color4f::new(0.0, 0.0, 0.0, 1.0), None);
+        paint.set_anti_alias(false);
+        paint.set_style(paint::Style::Stroke);
+        paint.set_stroke_width(4.0);
+        paint.set_stroke_cap(paint::Cap::Round);
+        self.canvas.draw_line(from.into(), to.into(), &paint);
     }
 
 }
 
 impl Deref for PaintCanvas<'_> {
 
-    type Target = Image;
+    type Target = Bitmap;
 
     fn deref(&self) -> &Self::Target {
-        &self.image
+        &self.bitmap
     }
 
 }
