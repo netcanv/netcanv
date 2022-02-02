2ac741cbb0f3b26aa863df17ae1302e01975eab8
devildefu <19360703+devildefu@users.noreply.github.com>
2022-02-02 18:17:28 +0100

    Clamp mouse wheel values only for wasm32

--- src/ui/input.rs
+++ src/ui/input.rs
@@ -214,12 +214,24 @@ impl Input {
 
          WindowEvent::MouseWheel { delta, .. } => {
             use crate::backend::winit::event::MouseScrollDelta::*;
-            self.mouse_scroll = match *delta {
-               LineDelta(x, y) => Vector::new(x.clamp(-1.0, 1.0), y.clamp(-1.0, 1.0)),
-               PixelDelta(PhysicalPosition { x, y }) => {
-                  Vector::new(x.clamp(-1.0, 1.0) as f32, y.clamp(-1.0, 1.0) as f32)
-               }
-            };
+
+            #[cfg(not(target_arch = "wasm32"))]
+            {
+               self.mouse_scroll = match *delta {
+                  LineDelta(x, y) => Vector::new(x, y),
+                  PixelDelta(PhysicalPosition { x, y }) => Vector::new(x as f32, y as f32),
+               };
+            }
+
+            #[cfg(target_arch = "wasm32")]
+            {
+               self.mouse_scroll = match *delta {
+                  LineDelta(x, y) => Vector::new(x.clamp(-1.0, 1.0), y.clamp(-1.0, 1.0)),
+                  PixelDelta(PhysicalPosition { x, y }) => {
+                     Vector::new(x.clamp(-1.0, 1.0) as f32, y.clamp(-1.0, 1.0) as f32)
+                  }
+               };
+            }
          }
 
          WindowEvent::ReceivedCharacter(c) => self.char_buffer.push(*c),
