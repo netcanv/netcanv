9ff0e918a6f0fac3c1156b7fd17f80716c4c53e2
devildefu <19360703+devildefu@users.noreply.github.com>
2024-08-01 18:17:41 +0200

    Make copying room id asynchronous (#183)
    Add new function `clipboard::copy_string_async()` and use it to postpone
    copying the room id until clipboard mutex is unlocked. It's not a
    perfect solution, because user might not realize the room id will be
    copied later after pressing the button, but it's better than frozen UI.
    
    Closes #168

--- src/clipboard.rs
+++ src/clipboard.rs
@@ -9,21 +9,35 @@ use once_cell::sync::Lazy;
 
 use crate::Error;
 
-static CLIPBOARD: Lazy<Mutex<Option<Clipboard>>> = Lazy::new(|| Mutex::new(None));
+struct ClipboardState {
+   string: Mutex<Option<String>>,
+   clipboard: Mutex<Option<Clipboard>>,
+}
+
+impl ClipboardState {
+   fn new() -> Self {
+      Self {
+         string: Mutex::new(None),
+         clipboard: Mutex::new(None),
+      }
+   }
+}
+
+static CLIPBOARD_STATE: Lazy<ClipboardState> = Lazy::new(|| ClipboardState::new());
 
 /// Initializes the clipboard in a platform-specific way.
 #[allow(unused)]
 pub fn init() -> netcanv::Result<()> {
    profiling::scope!("clipboard::init");
 
-   let mut clipboard = CLIPBOARD.lock().unwrap();
+   let mut clipboard = CLIPBOARD_STATE.clipboard.lock().unwrap();
    *clipboard = Some(Clipboard::new()?);
    Ok(())
 }
 
 /// Copies the provided string into the clipboard.
 pub fn copy_string(string: String) -> netcanv::Result<()> {
-   let mut clipboard = CLIPBOARD.lock().unwrap();
+   let mut clipboard = CLIPBOARD_STATE.clipboard.lock().unwrap();
    if let Some(clipboard) = &mut *clipboard {
       clipboard.set_text(string).map_err(|e| Error::CannotSaveToClipboard {
          error: e.to_string(),
@@ -34,9 +48,42 @@ pub fn copy_string(string: String) -> netcanv::Result<()> {
    }
 }
 
+/// Copies the provided string into the clipboard asynchronously, by updating given string
+/// in internal state and then copying it on separate thread.
+pub async fn copy_string_async(string: String) -> netcanv::Result<()> {
+   // Update string to copy
+   {
+      let mut state_string = CLIPBOARD_STATE.string.lock().unwrap();
+      *state_string = Some(string);
+   }
+
+   tokio::task::spawn_blocking(move || {
+      // Wait for clipboard's mutex to unlock first, because by the time the clipboard unlocks,
+      // string that we have to copy could have changed already.
+      let mut clipboard = CLIPBOARD_STATE.clipboard.lock().unwrap();
+      let mut string = CLIPBOARD_STATE.string.lock().unwrap();
+
+      if let Some(clipboard) = &mut *clipboard {
+         if let Some(string_to_copy) = (*string).take() {
+            tracing::trace!("copying string into the clipboard");
+            clipboard.set_text(&string_to_copy).map_err(|e| Error::CannotSaveToClipboard {
+               error: e.to_string(),
+            })?;
+            tracing::trace!("string copied");
+            Ok(())
+         } else {
+            Ok(())
+         }
+      } else {
+         Err(Error::ClipboardWasNotInitialized)
+      }
+   })
+   .await?
+}
+
 /// Copies the provided image into the clipboard.
 pub fn copy_image(image: RgbaImage) -> netcanv::Result<()> {
-   let mut clipboard = CLIPBOARD.lock().unwrap();
+   let mut clipboard = CLIPBOARD_STATE.clipboard.lock().unwrap();
    if let Some(clipboard) = &mut *clipboard {
       clipboard
          .set_image(ImageData {
@@ -55,7 +102,7 @@ pub fn copy_image(image: RgbaImage) -> netcanv::Result<()> {
 
 /// Pastes the contents of the clipboard into a string.
 pub fn paste_string() -> netcanv::Result<String> {
-   let mut clipboard = CLIPBOARD.lock().unwrap();
+   let mut clipboard = CLIPBOARD_STATE.clipboard.lock().unwrap();
    if let Some(clipboard) = &mut *clipboard {
       Ok(clipboard.get_text().map_err(|e| {
          if let arboard::Error::ContentNotAvailable = e {
@@ -70,7 +117,7 @@ pub fn paste_string() -> netcanv::Result<String> {
 }
 
 pub fn paste_image() -> netcanv::Result<RgbaImage> {
-   let mut clipboard = CLIPBOARD.lock().unwrap();
+   let mut clipboard = CLIPBOARD_STATE.clipboard.lock().unwrap();
    if let Some(clipboard) = &mut *clipboard {
       let image = clipboard
          .get_image()
